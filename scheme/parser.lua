a9b1685d7937da3aaea20767bd3be52cf8abcf4f
Lukas Niederbremer <Lukas.Niederbremer@gmail.com>
2016-02-08 22:32:34 +0100

    Hopefully fix parser for good
    Only reads precisely as much as it needs to read, so that mixing
    (read-*) and (read) on input ports neither corrupts the parser not eats
    data that doesn't need to be eaten.

--- scheme/parser.lua
+++ scheme/parser.lua
@@ -13,7 +13,6 @@ local function parser_new(source)
         source = source,
         line = 1,
         col = 1,
-        lastc = nil,
 
         store_lexical_information = true
     }
@@ -33,9 +32,17 @@ function parser.new_from_string(str)
     })
 end
 
+function stringparser_methods:last()
+    return sub(self.input, self.pos, self.pos)
+end
+
+function stringparser_methods:peek()
+    return sub(self.input, self.pos + 1, self.pos + 1)
+end
+
 function stringparser_methods:advance()
-    if self.lastc then
-        if self.lastc == "\n" then
+    if self:last() then
+        if self:last() == "\n" then
             self.line = self.line + 1
             self.col  = 1
         else
@@ -44,17 +51,13 @@ function stringparser_methods:advance()
     end
 
     self.pos = self.pos + 1
-    self.lastc = sub(self.input, self.pos, self.pos)
 
-    return self.lastc
+    return self:last()
 end
 
 function stringparser_methods:feed(chunk)
     self.input = self.input .. chunk
-
-    if self.lastc == "" then
-        self.lastc = sub(self.input, self.pos, self.pos)
-    end
+    self.pos = math.max(0, self.pos - 1) -- reset "EOF"
 end
 
 --[[
@@ -74,6 +77,7 @@ function parser.new_from_open_file(fp, name)
     local self = parser_new(name)
 
     self.file = fp
+    self.lastc = nil
     self.store_lexical_information = false
 
     return setmetatable(self, {
@@ -81,9 +85,21 @@ function parser.new_from_open_file(fp, name)
     })
 end
 
+function fileparser_methods:last()
+    return self.lastc
+end
+
+function fileparser_methods:peek()
+    local off = self.file:seek()
+    local c = self.file:read(1)
+
+    self.file:seek("set", off)
+    return c
+end
+
 function fileparser_methods:advance()
-    if self.lastc then
-        if self.lastc == "\n" then
+    if self:last() then
+        if self:last() == "\n" then
             self.line = self.line + 1
             self.col  = 1
         else
@@ -94,7 +110,7 @@ function fileparser_methods:advance()
     -- Let's hope the Lua implementation's file is buffered
     self.lastc = self.file:read(1) or ""
 
-    return self.lastc
+    return self:last()
 end
 
 --[[
@@ -118,8 +134,26 @@ function parser_methods:emit(fn, v, dl, dc)
     return v
 end
 
+function parser_methods:trim1()
+    local c = self:peek()
+    local in_comment = false
+
+    while c and c ~= "" and (c ~= "\n" and in_comment) or c:find("^[%s;]") do
+        if c == ";" then
+            in_comment = true
+        elseif c == "\n" then
+            in_comment = false
+        end
+
+        self:advance()
+        c = self:peek()
+    end
+
+    return c
+end
+
 function parser_methods:trim()
-    local c = self.lastc
+    local c = self:advance()
     local in_comment = false
 
     while c and c ~= "" and (c ~= "\n" and in_comment) or c:find("^[%s;]") do
@@ -152,10 +186,6 @@ function parser_methods:parse()
 end
 
 function parser_methods:parse_value()
-    if not self.lastc then
-        self:advance()
-    end
-
     local c = self:trim()
 
     if not c or c == "" then
@@ -176,15 +206,13 @@ function parser_methods:parse_value()
 
     elseif not c:find("[%d%s%(%)%#%[%]%'%`%,%;%\\]") then
         -- identifier
-        return self:parse_identifier(c)
+        return self:parse_identifier()
 
     elseif c:find("[%+%-%d%.]") then
-        return self:parse_number(c)
+        return self:parse_number()
 
     elseif c == "'" then
         local dl, dc = self.line, self.col
-
-        self:advance()
 
         return self:emit(types.list.new, {
             self:emit(types.ident.new, "quote", dl, dc),
@@ -192,8 +220,6 @@ function parser_methods:parse_value()
 
     elseif c == "`" then
         local dl, dc = self.line, self.col
-
-        self:advance()
 
         return self:emit(types.list.new, {
             self:emit(types.ident.new, "quasiquote", dl, dc),
@@ -221,23 +247,23 @@ end
 
 function parser_methods:parse_list()
     local dl, dc = self.line, self.col
-
+    local c
     local list = {}
-    local c = self:advance()
 
     while true do
-        c = self:trim()
+        c = self:trim1()
 
         if c == ")" or c == "" then
+            self:trim1()
             break
-        else
-            table.insert(list, self:parse_value())
         end
+
+        table.insert(list, self:parse_value())
     end
 
     if c ~= ")" then
         -- Hit EOF
-        self:err("expected \")\", found <eof> instead")
+        self:err("expected \")\", found %q instead", c == "" and "<eof>" or c)
     end
 
     self:advance()
@@ -250,13 +276,9 @@ function parser_methods:parse_hash()
     local c = self:advance()
 
     if c == "t" or c == "T" then
-        self:advance()
-
         return self:emit(types.boolean.new, true, dl, dc)
 
     elseif c == "f" or c == "F" then
-        self:advance()
-
         return self:emit(types.boolean.new, false, dl, dc)
 
     elseif c == "\\" then
@@ -267,17 +289,20 @@ function parser_methods:parse_hash()
         end
 
         if not c:find("[%s%w]") then
-            self:advance()
-
             return self:emit(types.char.new, c, dl, dc)
         else
             local buf = ""
 
-            repeat
+            while true do
                 buf = buf .. c
 
+                local p = self:peek()
+                if not p:find("[%a%x]") then
+                    break
+                end
+
                 c = self:advance()
-            until not c:find("[%a%x]")
+            end
 
             if #buf == 1 then
                 return self:emit(types.char.new, buf, dl, dc)
@@ -301,15 +326,11 @@ function parser_methods:parse_hash()
             end
         end
     elseif c == "'" then
-        self:advance()
-
         return self:emit(types.list.new, {
             self:emit(types.ident.new, "syntax", dl, dc),
             self:parse_value()}, dl, dc)
 
     elseif c == "`" then
-        self:advance()
-
         return self:emit(types.list.new, {
             self:emit(types.ident.new, "quasisyntax", dl, dc),
             self:parse_value()}, dl, dc)
@@ -356,20 +377,24 @@ function parser_methods:parse_string()
         self:err("expected end of string, found <eof> instead")
     end
 
-    self:advance()
-
     return self:emit(types.str.new, buf, dl, dc)
 end
 
-function parser_methods:parse_identifier(c)
+function parser_methods:parse_identifier()
     local buf = ""
     local dl, dc = self.line, self.col
 
-    repeat
+    local c = self:last()
+
+    while true do
         buf = buf .. c
 
+        if c == "" or self:peek():find("[%s%(%)%#%[%]%'%`%,%;%\\]") then
+            break
+        end
+
         c = self:advance()
-    until c == "" or c:find("[%s%(%)%#%[%]%'%`%,%;%\\]")
+    end
 
     local num = tonumber(buf)
     if num then
@@ -379,15 +404,21 @@ function parser_methods:parse_identifier(c)
     end
 end
 
-function parser_methods:parse_number(n)
+function parser_methods:parse_number()
     local buf = ""
     local dl, dc = self.line, self.col
 
-    repeat
+    local n = self:last()
+
+    while true do
         buf = buf .. n
 
+        if not self:peek():find("[%.%d%xeExX]") then
+            break
+        end
+
         n = self:advance()
-    until not n:find("[%.%d%xeExX]")
+    end
 
     local num = tonumber(buf)
     if not num then
